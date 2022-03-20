655cad9a84424245baeb201b8b9410d2852425a9
Oladepo Olabayoji <bayoji7@gmail.com>
2022-03-20 22:59:18 +0100

    remove console logs

--- src/components/UI/Success.jsx
+++ src/components/UI/Success.jsx
@@ -88,11 +88,9 @@ export default function Success() {
               <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
                 <button
                   type="button"
-                  className="mt-3 w-full inline-flex justify-center
-                  rounded-md border border-gray-300 shadow-sm px-4 py-2
-                   bg-white text-base font-medium text-black
-                    hover:bg-gray-50 focus:outline-none focus:ring-2
-                     focus:ring-offset-2  sm:mt-0
+                  className="mt-3 text-white w-full inline-flex justify-center
+                  rounded-md px-4 py-2 bg-my-primary text-base font-medium 
+                   hover:bg-my-primary/[0.6]  sm:mt-0
                       sm:ml-3 sm:w-auto sm:text-sm"
                   onClick={() => close()}
                   ref={cancelButtonRef}
